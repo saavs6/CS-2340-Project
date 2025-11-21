a471f0a9ef1e334cd27717fba0a195a113c31514
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-11-20 22:42:42 -0500

    update url and views in the backend

--- jobapp/home/urls.py
+++ jobapp/home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('admin-tools/', views.admin_dashboard, name='home.admin_dashboard'),
+    path('admin-tools/export/', views.export_csv, name='home.export_csv'),
 ]
